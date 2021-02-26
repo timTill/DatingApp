8ce1c607b3685dc122431e37f9c27398404f38ce
Szabi DEV <felszabolcs@gmail.com>
2021-02-26 15:04:33 +0100

    Members UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
+//    this.getUsers();
     this.setCurrentUser();
   }
 
